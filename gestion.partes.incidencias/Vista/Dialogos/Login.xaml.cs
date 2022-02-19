4ab07dded2c50e113019e1b3b9235243aa3c78c7
Aitor Dominguez <aitordominguez1997@gmail.com>
2022-02-19 11:28:48 +0100

    Se implementa el Login de la aplicación

--- gestion.partes.incidencias/Vista/Dialogos/Login.xaml.cs
+++ gestion.partes.incidencias/Vista/Dialogos/Login.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using gestion.partes.incidencias.Modelo;
+using gestion.partes.incidencias.MVVM;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace gestion.partes.incidencias.Vista.Dialogos
 {
@@ -20,9 +11,33 @@ namespace gestion.partes.incidencias.Vista.Dialogos
     /// </summary>
     public partial class Login : Window
     {
+        private tfgEntities tfgEnt;
+        private MVProfesor mvProfesor;
         public Login()
         {
             InitializeComponent();
+            tfgEnt = new tfgEntities();
+            mvProfesor = new MVProfesor(tfgEnt);
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            String usr = txtUser.Text;
+            String pass = txtPassword.Password;
+
+            if (mvProfesor.login(usr, pass))
+            {
+             //   MainWindow ventanaPrincipal = new MainWindow(invEnt, serverUsr.usuLogin);
+             //   ventanaPrincipal.Show();
+                this.Close();
+            }
+            else
+            {
+                txtUser.BorderBrush = new SolidColorBrush(Colors.Red);
+                txtPassword.BorderBrush = new SolidColorBrush(Colors.Red);
+                txtUser.Foreground = new SolidColorBrush(Colors.Red);
+                txtPassword.Foreground = new SolidColorBrush(Colors.Red);
+            }
         }
     }
 }
